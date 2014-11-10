3c759a05b3dcbfa2d6b40fc33592989aa9ad6b65
ibhutani <ibhutani@sfu.ca>
2014-11-09 22:30:38 -0800

    renamed possessions

--- web2py/applications/StuffShare/models/db.py
+++ web2py/applications/StuffShare/models/db.py
@@ -1,5 +1,5 @@
-# db = DAL('mysql://clan10:clan10@mysql.server/clan10$stuffshare') #this connects to the mysql server running on python anywhere; will not work unless the app is running on pythonanywhere
-db = DAL('mysql://clan10:clan10@localhost:3306/stuffshare') #connects to local mysql server with database: "stuffshare" on port: 3306 with username: clan10 and password: clan10
+db = DAL('mysql://clan10:clan10@mysql.server/clan10$stuffshare') #this connects to the mysql server running on python anywhere; will not work unless the app is running on pythonanywhere
+# db = DAL('mysql://clan10:clan10@localhost:3306/stuffshare') #connects to local mysql server with database: "stuffshare" on port: 3306 with username: clan10 and password: clan10
 from gluon.tools import Auth
 auth = Auth(db)
 auth.define_tables(username=False,signature=False)
@@ -21,10 +21,10 @@ db.possessions.quality.requires=IS_IN_SET(('Poor','Mediocre','Average', 'Good', 
 db.possessions.id.readable = False
 db.possessions.user_id.readable = False
 
-#this didn't work on my local mysql database, not sure why
-# db.define_table('friends',
-#     Field('id', 'integer', unique=True, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC()]),
-#     Field('user_id', 'integer', db.auth_user, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC(), IS_IN_DB(db, db.auth_user.id)]),
-#     Field('friend_id', 'integer', db.auth_user, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC(), IS_IN_DB(db, db.auth_user.id)]))
-#
-# db.friends.id.readable = False
+# this didn't work on my local mysql database, not sure why
+db.define_table('friends',
+    Field('id', 'integer', unique=True, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC()]),
+    Field('user_id', 'integer', db.auth_user, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC(), IS_IN_DB(db, db.auth_user.id)]),
+    Field('friend_id', 'integer', db.auth_user, requires=[IS_NOT_EMPTY(), IS_ALPHANUMERIC(), IS_IN_DB(db, db.auth_user.id)]))
+
+db.friends.id.readable = False
