61cf7ae19deefc662862485a2f026fc81f618e37
mmoo <mmoo@sfu.ca>
2014-11-10 00:54:05 -0800

    Fixed whitespace, added SQLITE connection string for offline testing.  Removed language files.

--- web2py/applications/StuffShare/models/db.py
+++ web2py/applications/StuffShare/models/db.py
@@ -1,8 +1,11 @@
 #db = DAL('mysql://clan10:clan10@mysql.server/clan10$stuffshare') #this connects to the mysql server running on python anywhere; will not work unless the app is running on pythonanywhere
-db = DAL('mysql://root:root@localhost:3306/stuffshare') #connects to local mysql server with database: "stuffshare" on port: 3306 with username: clan10 and password: clan10
+#This uses a local data-based sql server, PLEASE UPLOAD TO PYTHONANYWHERE and TEST ON SERVER BEFORE COMMITTING!
+# ALSO PLEASE COMMENT THIS AND UNCOMMENT THE PROPER MYSQL STRING
+db = DAL('sqlite://storage.db')
+
 from gluon.tools import Auth
 auth = Auth(db)
-auth.define_tables(username=False,signature=False)
+auth.define_tables(username=False, signature=False)
 
 auth.settings.actions_disabled.append('request_reset_password')
 auth.settings.actions_disabled.append('retrieve_username')
@@ -17,12 +20,12 @@ db.define_table('possessions',
     Field('return_date', 'date'),
     Field('picture', 'upload'))
 
-db.possessions.user_id.requires=IS_NOT_EMPTY()
-db.possessions.user_id.requires=IS_IN_DB(db, db.auth_user.id)
-db.possessions.item_name.requires=IS_NOT_EMPTY()
-db.possessions.notes.requires=IS_NOT_EMPTY()
-db.possessions.quality.requires=IS_NOT_EMPTY()
-db.possessions.return_date.requires=IS_NOT_EMPTY()
+db.possessions.user_id.requires = IS_NOT_EMPTY()
+db.possessions.user_id.requires = IS_IN_DB(db, db.auth_user.id)
+db.possessions.item_name.requires = IS_NOT_EMPTY()
+db.possessions.notes.requires = IS_NOT_EMPTY()
+db.possessions.quality.requires = IS_NOT_EMPTY()
+db.possessions.return_date.requires = IS_NOT_EMPTY()
 
 db.possessions.quality.requires=IS_IN_SET(('Poor','Mediocre','Average', 'Good', 'Excellent', 'Like New'))
 db.possessions.id.readable = False
@@ -34,11 +37,11 @@ db.define_table('friends',
     Field('user_id', 'integer'),
     Field('friend_id', 'integer'))
 
-db.friends.user_id.requires=IS_NOT_EMPTY()
-db.friends.user_id.requires=IS_ALPHANUMERIC()
-db.friends.user_id.requires=IS_IN_DB(db, db.auth_user.id)
-db.friends.friend_id.requires=IS_NOT_EMPTY()
-db.friends.friend_id.requires=IS_ALPHANUMERIC()
-db.friends.friend_id.requires=IS_IN_DB(db, db.auth_user.id)
+db.friends.user_id.requires = IS_NOT_EMPTY()
+db.friends.user_id.requires = IS_ALPHANUMERIC()
+db.friends.user_id.requires = IS_IN_DB(db, db.auth_user.id)
+db.friends.friend_id.requires = IS_NOT_EMPTY()
+db.friends.friend_id.requires = IS_ALPHANUMERIC()
+db.friends.friend_id.requires = IS_IN_DB(db, db.auth_user.id)
 
 db.friends.id.readable = False
