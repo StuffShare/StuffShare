6782028d2295b8379bb84c59a6f36c859382dd32
mmoo <mmoo@sfu.ca>
2014-11-17 23:04:19 -0800

    Delete unnecessary examples and layout files.

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a6df6490a2d9ca58$e5f734baeaedab0117c33f117976ab96b0f0c7fc"
+password="pbkdf2(1000,20,sha512)$afe8d2dfb87ff83f$65fec8268911829443f87271dcf5f4fb007a85df"
