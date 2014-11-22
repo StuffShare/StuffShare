e3afb2d946785ffc4b145a42ad84a60b024d87e8
Gary Williams <gewillia@sfu.ca>
2014-11-21 23:10:14 -0800

    initial version

---
+++ web2py/applications/StuffShare/controllers/isbndb.py
@@ -0,0 +1,18 @@
+__author__ = 'gwilliams'
+
+import urllib2
+import json
+
+ISBNDB_ACCOUNT_NUMBER = 'I7G9FT9U' # this account number is good for 500 queries per day
+
+def get_book_info(isbn):
+	url = 'http://isbndb.com/api/v2/json/' + ISBNDB_ACCOUNT_NUMBER + '/book/' + isbn
+	response = urllib2.urlopen(url)
+	book_info = json.loads(response.read())
+
+def get_book_title(isbn):
+	book_info = get_book_info(isbn)
+
+	# the following statement assumes the isbn was found.
+	# we should probably test for error conditions.
+	return book_info['data'][0]['title_latin']
