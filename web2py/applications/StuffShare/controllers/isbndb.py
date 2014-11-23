6bd92a4db1631ea05151eb2210083bff5e18df1b
Gary Williams <gewillia@sfu.ca>
2014-11-22 16:19:43 -0800

    wasn't returning the json object

--- web2py/applications/StuffShare/controllers/isbndb.py
+++ web2py/applications/StuffShare/controllers/isbndb.py
@@ -6,9 +6,10 @@ import json
 ISBNDB_ACCOUNT_NUMBER = 'I7G9FT9U' # this account number is good for 500 queries per day
 
 def get_book_info(isbn):
-	url = 'http://isbndb.com/api/v2/json/' + ISBNDB_ACCOUNT_NUMBER + '/book/' + isbn
-	response = urllib2.urlopen(url)
-	book_info = json.loads(response.read())
+    url = 'http://isbndb.com/api/v2/json/' + ISBNDB_ACCOUNT_NUMBER + '/book/' + isbn
+    response = urllib2.urlopen(url)
+    book_info = json.loads(response.read())
+    return book_info
 
 def get_book_title(isbn):
 	book_info = get_book_info(isbn)
