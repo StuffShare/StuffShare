671dc9afa3f426e5487043e2db15ae859a7249a6
Gary Williams <gewillia@sfu.ca>
2014-11-30 20:36:15 -0800

    openlibrary.py now uses isbn.py

--- web2py/applications/StuffShare/controllers/openlibrary.py
+++ web2py/applications/StuffShare/controllers/openlibrary.py
@@ -3,12 +3,22 @@ __author__ = 'Gary Williams'
 import urllib2
 import json
 
-def get_book_info(isbn):
-    url = 'https://openlibrary.org/api/books?jscmd=data&bibkeys=ISBN:' + isbn
+def get_book_info(some_isbn):
+    if not isbn.is_valid_isbn(some_isbn):
+        return some_isbn + " is not a valid ISBN"
+
+    isbn10, isbn13 = isbn.fix_isbn(some_isbn)
+
+    url = 'https://openlibrary.org/api/books?jscmd=data&bibkeys=ISBN:' + isbn10
     response = urllib2.urlopen(url)
     json_object = json.loads(response.read())
     return json_object
 
 def get_book_title(isbn):
-    book_info = get_book_info(isbn)
+    if not isbn.is_valid_isbn(some_isbn):
+        return some_isbn + " is not a valid ISBN"
+
+    isbn10, isbn13 = isbn.fix_isbn(some_isbn)
+
+    book_info = get_book_info(isbn10)
     return book_info['title']
