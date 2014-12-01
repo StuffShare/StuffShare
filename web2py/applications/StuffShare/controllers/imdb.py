33d8687cf2c272a3da0f5cb0acbc1a8f81a3c2a3
Gary Williams <gewillia@sfu.ca>
2014-11-30 19:08:17 -0800

    bugfixes

--- web2py/applications/StuffShare/controllers/imdb.py
+++ web2py/applications/StuffShare/controllers/imdb.py
@@ -3,12 +3,30 @@ __author__ = 'Gary Williams'
 import urllib2
 import json
 
-# this will return all the title matches
-# for example output, see: http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q=Shawshank
-# we'll have to retrieve the id (e.g. tt0293927) of the appropriate match to store in our database
-
-def find_movie_by_title(title):
-    url = 'http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q=' + title
+def find_movies_by_title(title):
+    url = 'http://www.omdbapi.com/?t=' + title.replace(' ', '+') + '&y=&plot=short&r=json'
     response = urllib2.urlopen(url)
     json_object = json.loads(response.read())
     return json_object
+
+def find_movies_by_title_and_year(title, year):
+    url = 'http://www.omdbapi.com/?t=' + title.replace(' ', '+') + '&y=' + year + '&plot=short&r=json'
+    response = urllib2.urlopen(url)
+    json_object = json.loads(response.read())
+    return json_object
+
+def get_movie_details_by_id(id):
+    if not (id.startswith('tt')):
+        id = 'tt' + id
+
+    url = 'http://www.omdbapi.com/?i=' + id + '&plot=short&r=json'
+    response = urllib2.urlopen(url)
+    json_object = json.loads(response.read())
+    return json_object
+
+if __name__ == "__main__":
+    print find_movies_by_title('The Shawshank Redemption')
+    print ''
+    print find_movies_by_title_and_year('Star Trek', '1994')
+    print ''
+    print get_movie_details_by_id('tt0060666')
